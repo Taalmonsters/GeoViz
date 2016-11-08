cc1c9ffb693d0aa3e1d5a914e2397f947fc492fd
matjemeisje <matje@taalmonsters.nl>
2016-11-08 21:15:42 +0100

    Add assets

--- app/assets/javascripts/geo_viz/application.js
+++ app/assets/javascripts/geo_viz/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function processAnnotationSelection(parentId, selectedElements) {
+	addClassToSelected(parentId, selectedElements, "selected-for-annotation");
+	
+}
