b10f1afb153ee5ff79eacaf15dcd6beb4185d655
matjemeisje <matje@taalmonsters.nl>
2016-11-29 13:56:19 +0100

    Updated metadata loader to nested_metadata 0.0.3

--- app/services/geo_viz/geo_parser/metadata_loader.rb
+++ app/services/geo_viz/geo_parser/metadata_loader.rb
@@ -13,36 +13,41 @@ module GeoViz
       geoparser_keys = ["id", "name", "latitude", "longitude", "population", "gazetteer", "gazref", "type", "country"]
       ActiveRecord::Base.transaction do
         fields = self.add_groups_and_keys({ "Annotations" => geoparser_keys, "GeoParser" => geoparser_keys })
+        NestedMetadata::MetadataGroup.with_name("Annotations").first.update_attribute(:group_keys_into_entity, true)
         NestedMetadata::MetadataGroup.with_name("GeoParser").first.update_attribute(:group_keys_into_entity, true)
+        metadata_group_id = NestedMetadata::MetadataGroup.with_name("GeoParser").first.id
         document_id = self.get_last_document_id
+        source_document_id = self.get_last_source_document_id
+        entity_mention_id = self.get_last_entity_mention_id
         Dir.entries(input_dir).select{|entry| !entry.start_with?('.') && entry.end_with?('.xml') && !entry.end_with?('.gaz.xml') }.each do |entry|
           file = "#{input_dir}/#{entry}"
           title = entry.sub(/\.xml$/,"")
           gaz_file = "#{input_dir}/#{title}.gaz.xml"
           generated_id = Digest::MD5.hexdigest(title)
           document_id += 1
-          entity_count = 0
-          self.add_document(document_id, generated_id, title)
+          source_document_id += 1
+          self.add_document(source_document_id, document_id, generated_id, title)
           self.set_blacklab_pid(document_id, title)
           main_xml_doc = Nokogiri::XML(File.read(file))
           xml_doc = Nokogiri::XML(File.read(gaz_file))
           xml_doc.css("placename").each do |placename|
-            entity_count += 1
+            entity_mention_id += 1
+            self.add_entity_mention_to_document(source_document_id, metadata_group_id, entity_mention_id, "location")
             place = placename.css("place").first
-            self.add_metadata_to_document(document_id, fields["GeoParser"]["id"], [self.get_placename_id(placename["id"], main_xml_doc)], entity_count) if placename["id"]
-            self.add_metadata_to_document(document_id, fields["GeoParser"]["name"], [placename["name"]], entity_count) if placename["name"]
-            self.add_metadata_to_document(document_id, fields["GeoParser"]["latitude"], [place["lat"].to_f], entity_count) if place["lat"]
-            self.add_metadata_to_document(document_id, fields["GeoParser"]["longitude"], [place["long"].to_f], entity_count) if place["long"]
-            self.add_metadata_to_document(document_id, fields["GeoParser"]["population"], [place["pop"].to_i], entity_count) if place["pop"]
+            self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["id"], self.get_placename_id(placename["id"], main_xml_doc)) if placename["id"]
+            self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["name"], placename["name"]) if placename["name"]
+            self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["latitude"], place["lat"].to_f) if place["lat"]
+            self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["longitude"], place["long"].to_f) if place["long"]
+            self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["population"], place["pop"].to_i) if place["pop"]
             if place["gazetteer"]
-              self.add_metadata_to_document(document_id, fields["GeoParser"]["gazetteer"], [place["gazetteer"]], entity_count)
-              self.add_metadata_to_document(document_id, fields["GeoParser"]["gazref"], [place["gazref"]], entity_count) if place["gazref"]
+              self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["gazetteer"], place["gazetteer"])
+              self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["gazref"], place["gazref"]) if place["gazref"]
             elsif place["gazref"]
-              self.add_metadata_to_document(document_id, fields["GeoParser"]["gazetteer"], [place["gazref"].split(":")[0]], entity_count)
-              self.add_metadata_to_document(document_id, fields["GeoParser"]["gazref"], [place["gazref"]], entity_count)
+              self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["gazetteer"], place["gazref"].split(":")[0])
+              self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["gazref"], place["gazref"].split(":",2)[1])
             end
-            self.add_metadata_to_document(document_id, fields["GeoParser"]["type"], [place["type"]], entity_count) if place["type"]
-            self.add_metadata_to_document(document_id, fields["GeoParser"]["country"], [place["in-cc"]], entity_count) if place["in-cc"]
+            self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["type"], place["type"]) if place["type"]
+            self.add_metadatum_to_entity_mention(source_document_id, entity_mention_id, fields["GeoParser"]["country"], place["in-cc"]) if place["in-cc"]
           end
           FileUtils.mv(file, "#{output_dir}/#{entry}")
           FileUtils.mv(gaz_file, "#{output_dir}/#{title}.gaz.xml")
