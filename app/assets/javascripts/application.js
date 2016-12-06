f6781deacf56285d731c6a3cf6e78b2f5f1e9d15
matjemeisje <matje@taalmonsters.nl>
2016-12-06 21:37:11 +0100

    Bugfix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require bootstrap
 //= require taalmonsters/maps/application
 //= require geo_viz/geo_viz
 //= require_tree .
