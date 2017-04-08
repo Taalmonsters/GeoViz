d74233b2ddb207335c5d31a6495b916018a97efa
matjemeisje <matje@taalmonsters.nl>
2017-04-08 19:41:40 +0200

    Fix annotation layout

--- app/helpers/geo_viz/application_helper.rb
+++ app/helpers/geo_viz/application_helper.rb
@@ -68,7 +68,6 @@ module GeoViz
     end
 
     def searchDbPedia(query)
-        puts "SEARCHING"
         return Dbpedia.sparql.query("PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
             "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n"+
             "PREFIX dbo: <http://dbpedia.org/ontology/>\n"+
