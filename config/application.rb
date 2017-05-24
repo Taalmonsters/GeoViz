16e219da928d9422f575b14800b4eb947e36c739
matjemeisje <matje@taalmonsters.nl>
2017-05-24 14:39:56 +0200

    Bugfixes

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module GeoViz
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    config.encoding = "utf-8"
     
     config.app_name = "GeoViz"
     config.app_version = File.read(Rails.root.join("VERSION.txt"))
